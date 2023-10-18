6c3a234fc0fc55586a89593bcb7beb92b3641eea
Will Liao <will.liao1@hp.com>
2023-10-17 18:19:59 -0700

    done

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 //---------------------------------
 // Your own functions here
 //---------------------------------
-
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
 
 function sayHello() {
     //do something
